c21105cafc60eed1c760273e4b97ecf8969b038c
Paul Singh <paul@resultsjunkies.com>
2020-09-10 14:09:33 -0400

    Let's try rewriting the decorator. Again.

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,16 +1,20 @@
-module Spree::ShipmentDecorator
-  def self.exportable
-    query = order(:updated_at).joins(:order).merge(Spree::Order.complete).where.not(spree_shipments: { state: 'canceled' })
-    query = query.ready unless Spree::Config.shipstation_capture_at_notification
-    query
-  end
+module Spree::ShipmentDecorator  
+  # This is the place to define custom associations, delegations, scopes and
+  # other ActiveRecord stuff
+  def self.prepended(base)
+    def base.exportable
+      query = order(:updated_at).joins(:order).merge(Spree::Order.complete).where.not(spree_shipments: { state: 'canceled' })
+      query = query.ready unless Spree::Config.shipstation_capture_at_notification
+      query
+    end
 
-  def self.between(from, to)
-    joins(:order).where(
-      '(spree_shipments.updated_at > ? AND spree_shipments.updated_at < ?) OR
-      (spree_orders.updated_at > ? AND spree_orders.updated_at < ?)',
-      from, to, from, to
-    )
+    def base.between(from, to)
+      joins(:order).where(
+        '(spree_shipments.updated_at > ? AND spree_shipments.updated_at < ?) OR
+        (spree_orders.updated_at > ? AND spree_orders.updated_at < ?)',
+        from, to, from, to
+      )
+    end
   end
 
   Spree::Shipment.prepend self
